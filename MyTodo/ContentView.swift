9457018df97cecaa954f08a77e9a575493a4cfe3
Sangmin Kim <offiksm0903@naver.com>
2026-06-24 00:28:49 +0900

    Implement basic ListView

--- MyTodo/ContentView.swift
+++ MyTodo/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 import SwiftData
 
+final class ListItem: Identifiable {
+    var timeStamp: Date
+    
+    init(timeStamp: Date = Date()) {
+        self.timeStamp = timeStamp
+    }
+}
+
 struct ContentView: View {
+    @State private var items: [ListItem] = []
+    
     var body: some View {
-        Text("Hello World!")
+        NavigationStack {
+            List(items) { item in
+                Text("\(item.timeStamp.description)")
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: addItem) {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .navigationTitle("My Todo")
+        }
+    }
+    
+    func addItem() {
+        withAnimation {
+            items.append(ListItem())
+        }
     }
 }
 
